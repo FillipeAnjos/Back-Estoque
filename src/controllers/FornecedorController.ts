bbf868ffb6a27b2f05d50fdc58371b035ccf3606
FillipeAnjos <fillipe.anjos.2000@hotmail.com>
2022-01-16 11:41:48 -0300

    Inclusão do Listar Fornecedores

--- src/controllers/FornecedorController.ts
+++ src/controllers/FornecedorController.ts
@@ -15,6 +15,52 @@ class FornecedorController{
         
     }
 
+    async listarFornecedores() {
+
+        const fornecedorService = new FornecedorService();
+
+        const fornecedores = fornecedorService.listarFornecedores();
+
+        return fornecedores;
+        
+    }
+
+    async listarFornecedoresParam(request: Request, response: Response) {
+
+        const { filtro, dados } = request.body.param;
+
+        const fornecedorService = new FornecedorService();
+
+        const fornecedores = fornecedorService.listarFornecedoresParam({ filtro, dados });
+
+        return fornecedores;
+        
+    }
+
+    async excluirFornecedor(request: Request, response: Response) {
+
+        const { id } = request.body;
+
+        const fornecedorService = new FornecedorService();
+
+        const excluirFornecedor = fornecedorService.excluirFornecedor({ id });
+
+        return excluirFornecedor;
+
+    }
+
+    async editarFornecedor(request: Request, response: Response) {
+
+        const { id, nome, cnpj, razao, falarcom, endereco, numero, cel, email } = request.body.param;
+
+        const clienteService = new FornecedorService();
+
+        const editarFornecedor = clienteService.editarFornecedor({ id, nome, cnpj, razao, falarcom, endereco, numero, cel, email });
+
+        return editarFornecedor;
+
+    }
+
 }
 
 export { FornecedorController }
