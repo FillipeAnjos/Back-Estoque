4eef9f8798647843818f264d358f77ecef7e2d7a
FillipeAnjos <fillipe.anjos.2000@hotmail.com>
2022-06-11 21:30:40 -0300

    Correção server e routes

--- dist/server.js
+++ dist/server.js
@@ -3,15 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const routes_1 = require("./routes");
+import { router } from "./routes";
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-require("reflect-metadata");
+import "reflect-metadata";
 const app = (0, express_1.default)();
-require("./database");
+import "./database";
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.use(routes_1.router);
+app.use(router);
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log("Conectado com sucesso na porta: " + port));
 //# sourceMappingURL=server.js.map
