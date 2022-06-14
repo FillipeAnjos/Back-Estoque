b01d86c24337349e022e6c3298b168d61efdbb90
FillipeAnjos <fillipe.anjos.2000@hotmail.com>
2022-06-14 09:17:29 -0300

    Testativa Heroku teste

--- dist/src/controllers/EstoqueController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EstoqueController = void 0;
-var EstoqueController = (function () {
-    function EstoqueController() {
-    }
-    return EstoqueController;
-}());
-exports.EstoqueController = EstoqueController;
