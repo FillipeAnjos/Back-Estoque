39cb1b3da76a8e1d387de507e047a72f31fdac32
FillipeAnjos <fillipe.anjos.2000@hotmail.com>
2022-06-15 08:43:48 -0300

    Banco dinamico

---
+++ ormconfig.js
@@ -0,0 +1,11 @@
+console.log("DATABASE_URL: " + process.env.DATABASE_URL);
+module.exports = {
+    "type": "postgres",
+    "url": process.env.DATABASE_URL,
+    "migrations": ["./src/database/migrations/*.js"],
+    "entities": ["src/models/*.js"],
+    "cli": {
+        "migrationsDir": "src/database/migrations",
+        "entitiesDir": "src/models"
+    }
+}
