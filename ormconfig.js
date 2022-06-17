36014bafdcdb3007e2b85c0d70a9674e7ac76760
FillipeAnjos <fillipe.anjos.2000@hotmail.com>
2022-06-17 09:14:24 -0300

    Correção ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,12 @@
 console.log("DATABASE_URL: " + process.env.DATABASE_URL);
+console.log("MIGRATIONS: " + process.env.MIGRATIONS);
+console.log("ENTITIES: " + process.env.ENTITIES);
 module.exports = {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
-    "ssl": { rejectUnauthorized: false },
+    "ssl": null,
+    "migrationsRun": true,
+    "logging": true,
     "migrations": [process.env.MIGRATIONS],
     "entities": [process.env.ENTITIES],
     "cli": {
