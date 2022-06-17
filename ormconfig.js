2167bd54370757e42b9f6eeae7522d25b967ceaa
FillipeAnjos <fillipe.anjos.2000@hotmail.com>
2022-06-17 09:43:57 -0300

    Teste migration run

--- ormconfig.js
+++ ormconfig.js
@@ -3,14 +3,14 @@ console.log("MIGRATIONS: " + process.env.MIGRATIONS);
 console.log("ENTITIES: " + process.env.ENTITIES);
 console.log("MIGRATIONS_RUN: " + process.env.MIGRATIONS_RUN);
 console.log("LOGGING: " + process.env.LOGGING);
-console.log("---------------------------------------------------------------------------------------------------------");
+console.log("-------------------------------------------------------------------------------------------------------------------------------------------");
 
 module.exports = {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
     "ssl": { rejectUnauthorized: false },
-    "migrationsRun": process.env.MIGRATIONS_RUN,
-    "logging": process.env.LOGGING,
+    "migrationsRun": true,
+    "logging": true,
     "migrations": [process.env.MIGRATIONS],
     "entities": [process.env.ENTITIES],
     "cli": {
